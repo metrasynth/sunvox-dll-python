f25c206852581c3473dc66281316db283dcf7ce9
Matthew Scott <matt@11craft.com>
2020-02-14 11:41:23 -0600

    Sort by same order as in sunvox.h

--- sunvox/slot.py
+++ sunvox/slot.py
@@ -63,112 +63,16 @@ class Slot(object):
         self.allocation_map[self.process][i] = False
         return self.process.close_slot(i)
 
-    def connect_module(self, source, destination):
-        with self.locked():
-            return self.process.connect_module(self.number, source, destination)
+    def lock(self):
+        self.locks += 1
+        if self.locks == 1:
+            return self.process.lock_slot(self.number)
 
-    def disconnect_module(self, source, destination):
-        with self.locked():
-            return self.process.disconnect_module(self.number, source, destination)
-
-    def end_of_song(self):
-        return self.process.end_of_song(self.number)
-
-    def get_current_line(self):
-        return self.process.get_current_line(self.number)
-
-    def get_current_line2(self):
-        return self.process.get_current_line2(self.number)
-
-    def get_current_signal_level(self, channel):
-        return self.process.get_current_signal_level(self.number, channel)
-
-    def get_module_color(self, mod_num):
-        return self.process.get_module_color(self.number, mod_num)
-
-    def get_module_finetune(self, mod_num: c_int) -> c_uint32:
-        return self.process.get_module_finetune(self.number, mod_num)
-
-    def get_module_ctl_name(self, mod_num, ctl_num):
-        return self.process.get_module_ctl_name(self.number, mod_num, ctl_num)
-
-    def get_module_ctl_value(self, mod_num, ctl_num, scaled):
-        return self.process.get_module_ctl_value(self.number, mod_num, ctl_num, scaled)
-
-    def find_module(self, name: c_char_p) -> c_int:
-        return self.process.find_module(self.number, name)
-
-    def get_module_flags(self, mod_num):
-        return self.process.get_module_flags(self.number, mod_num)
-
-    def get_module_inputs(self, mod_num):
-        return self.process.get_module_inputs(self.number, mod_num)
-
-    def get_module_name(self, mod_num):
-        return self.process.get_module_name(self.number, mod_num)
-
-    def get_module_outputs(self, mod_num):
-        return self.process.get_module_outputs(self.number, mod_num)
-
-    def get_module_scope2(self, mod_num, channel, read_buf, samples_to_read):
-        return self.process.get_module_scope2(
-            self.number, mod_num, channel, read_buf, samples_to_read
-        )
-
-    def module_curve(
-        self,
-        mod_num: c_int,
-        curve_num: c_int,
-        data: POINTER(c_float),
-        len: c_int,
-        w: c_int,
-    ) -> c_int:
-        return self.module_curve(self.number, mod_num, curve_num, data, len, w)
-
-    def get_module_xy(self, mod_num):
-        return self.process.get_module_xy(self.number, mod_num)
-
-    def get_number_of_modules(self):
-        return self.process.get_number_of_modules(self.number)
-
-    def get_number_of_module_ctls(self, mod_num):
-        return self.process.get_number_of_module_ctls(self.number, mod_num)
-
-    def get_number_of_patterns(self):
-        return self.process.get_number_of_patterns(self.number)
-
-    def get_pattern_data(self, pat_num):
-        return self.process.get_pattern_data(self.number, pat_num)
-
-    def get_pattern_lines(self, pat_num):
-        return self.process.get_pattern_lines(self.number, pat_num)
-
-    def get_pattern_name(self, pat_num: c_int) -> c_char_p:
-        return self.process.get_pattern_name(self.number, pat_num)
-
-    def get_pattern_tracks(self, pat_num):
-        return self.process.get_pattern_tracks(self.number, pat_num)
-
-    def find_pattern(self, name: c_char_p) -> c_int:
-        return self.process.find_pattern(self.number, name)
-
-    def get_pattern_x(self, pat_num):
-        return self.process.get_pattern_x(self.number, pat_num)
-
-    def get_pattern_y(self, pat_num):
-        return self.process.get_pattern_y(self.number, pat_num)
-
-    def get_song_bpm(self):
-        return self.process.get_song_bpm(self.number)
-
-    def get_song_length_frames(self):
-        return self.process.get_song_length_frames(self.number)
-
-    def get_song_length_lines(self):
-        return self.process.get_song_length_lines(self.number)
-
-    def get_song_tpl(self):
-        return self.process.get_song_tpl(self.number)
+    def unlock(self):
+        if self.locks > 0:
+            self.locks -= 1
+            if self.locks == 0:
+                return self.process.unlock_slot(self.number)
 
     def load(self, file_or_name):
         if isinstance(file_or_name, str):
@@ -188,6 +92,86 @@ class Slot(object):
     def load_filename(self, filename):
         return self.process.load(self.number, filename)
 
+    def play(self):
+        return self.process.play(self.number)
+
+    def play_from_beginning(self):
+        return self.process.play_from_beginning(self.number)
+
+    def stop(self):
+        return self.process.stop(self.number)
+
+    def set_autostop(self, autostop):
+        return self.process.set_autostop(self.number, autostop)
+
+    def get_autostop(self) -> c_int:
+        return self.process.get_autostop(self.number)
+
+    def end_of_song(self):
+        return self.process.end_of_song(self.number)
+
+    def rewind(self, line_num):
+        return self.process.rewind(self.number, line_num)
+
+    def volume(self, vol):
+        return self.process.volume(self.number, vol)
+
+    def set_event_t(self, set: c_int, t: c_int) -> c_int:
+        return self.process.set_event_t(self.number, set, t)
+
+    def send_event(self, track_num, note, vel, module, ctl, ctl_val):
+        module_index = getattr(module, "index", None)
+        if module_index is not None:
+            module = module_index + 1
+        return self.process.send_event(
+            self.number, track_num, note, vel, module, ctl, ctl_val
+        )
+
+    def get_current_line(self):
+        return self.process.get_current_line(self.number)
+
+    def get_current_line2(self):
+        return self.process.get_current_line2(self.number)
+
+    def get_current_signal_level(self, channel):
+        return self.process.get_current_signal_level(self.number, channel)
+
+    def get_song_name(self):
+        return self.process.get_song_name(self.number)
+
+    def get_song_bpm(self):
+        return self.process.get_song_bpm(self.number)
+
+    def get_song_tpl(self):
+        return self.process.get_song_tpl(self.number)
+
+    def get_song_length_frames(self):
+        return self.process.get_song_length_frames(self.number)
+
+    def get_song_length_lines(self):
+        return self.process.get_song_length_lines(self.number)
+
+    def get_time_map(
+        self, start_line: c_int, len: c_int, dest: POINTER(c_uint32), flags: c_int
+    ):
+        return self.process.get_time_map(self.number, start_line, len, dest, flags)
+
+    def new_module(self, module_type, name, x, y, z):
+        with self.locked():
+            return self.process.new_module(self.number, module_type, name, x, y, z)
+
+    def remove_module(self, mod_num):
+        with self.locked():
+            return self.process.remove_module(self.number, mod_num)
+
+    def connect_module(self, source, destination):
+        with self.locked():
+            return self.process.connect_module(self.number, source, destination)
+
+    def disconnect_module(self, source, destination):
+        with self.locked():
+            return self.process.disconnect_module(self.number, source, destination)
+
     def load_module(self, file_name, x=512, y=512, z=512):
         value = None
         if isinstance(file_name, BytesIO):
@@ -203,37 +187,6 @@ class Slot(object):
                 self.number, value, len(value), x, y, z
             )
 
-    def lock(self):
-        self.locks += 1
-        if self.locks == 1:
-            return self.process.lock_slot(self.number)
-
-    def get_time_map(
-        self, start_line: c_int, len: c_int, dest: POINTER(c_uint32), flags: c_int
-    ):
-        return self.process.get_time_map(self.number, start_line, len, dest, flags)
-
-    def new_module(self, module_type, name, x, y, z):
-        with self.locked():
-            return self.process.new_module(self.number, module_type, name, x, y, z)
-
-    def pattern_mute(self, pat_num, mute):
-        with self.locked():
-            return self.process.pattern_mute(self.number, pat_num, mute)
-
-    def play(self):
-        return self.process.play(self.number)
-
-    def play_from_beginning(self):
-        return self.process.play_from_beginning(self.number)
-
-    def remove_module(self, mod_num):
-        with self.locked():
-            return self.process.remove_module(self.number, mod_num)
-
-    def rewind(self, line_num):
-        return self.process.rewind(self.number, line_num)
-
     def sampler_load(self, sampler_module, file_name, sample_slot):
         return self.process.sampler_load(
             self.number, sampler_module, file_name, sample_slot
@@ -244,34 +197,84 @@ class Slot(object):
             self.number, sampler_module, data, len(data), sample_slot
         )
 
-    def set_event_t(self, set: c_int, t: c_int) -> c_int:
-        return self.process.set_event_t(self.number, set, t)
+    def get_number_of_modules(self):
+        return self.process.get_number_of_modules(self.number)
 
-    def send_event(self, track_num, note, vel, module, ctl, ctl_val):
-        module_index = getattr(module, "index", None)
-        if module_index is not None:
-            module = module_index + 1
-        return self.process.send_event(
-            self.number, track_num, note, vel, module, ctl, ctl_val
+    def find_module(self, name: c_char_p) -> c_int:
+        return self.process.find_module(self.number, name)
+
+    def get_module_flags(self, mod_num):
+        return self.process.get_module_flags(self.number, mod_num)
+
+    def get_module_inputs(self, mod_num):
+        return self.process.get_module_inputs(self.number, mod_num)
+
+    def get_module_outputs(self, mod_num):
+        return self.process.get_module_outputs(self.number, mod_num)
+
+    def get_module_name(self, mod_num):
+        return self.process.get_module_name(self.number, mod_num)
+
+    def get_module_xy(self, mod_num):
+        return self.process.get_module_xy(self.number, mod_num)
+
+    def get_module_color(self, mod_num):
+        return self.process.get_module_color(self.number, mod_num)
+
+    def get_module_finetune(self, mod_num: c_int) -> c_uint32:
+        return self.process.get_module_finetune(self.number, mod_num)
+
+    def get_module_scope2(self, mod_num, channel, read_buf, samples_to_read):
+        return self.process.get_module_scope2(
+            self.number, mod_num, channel, read_buf, samples_to_read
         )
 
-    def set_autostop(self, autostop):
-        return self.process.set_autostop(self.number, autostop)
+    def module_curve(
+        self,
+        mod_num: c_int,
+        curve_num: c_int,
+        data: POINTER(c_float),
+        len: c_int,
+        w: c_int,
+    ) -> c_int:
+        return self.module_curve(self.number, mod_num, curve_num, data, len, w)
 
-    def get_autostop(self) -> c_int:
-        return self.process.get_autostop(self.number)
+    def get_number_of_module_ctls(self, mod_num):
+        return self.process.get_number_of_module_ctls(self.number, mod_num)
 
-    def stop(self):
-        return self.process.stop(self.number)
+    def get_module_ctl_name(self, mod_num, ctl_num):
+        return self.process.get_module_ctl_name(self.number, mod_num, ctl_num)
 
-    def volume(self, vol):
-        return self.process.volume(self.number, vol)
+    def get_module_ctl_value(self, mod_num, ctl_num, scaled):
+        return self.process.get_module_ctl_value(self.number, mod_num, ctl_num, scaled)
 
-    def unlock(self):
-        if self.locks > 0:
-            self.locks -= 1
-            if self.locks == 0:
-                return self.process.unlock_slot(self.number)
+    def get_number_of_patterns(self):
+        return self.process.get_number_of_patterns(self.number)
+
+    def find_pattern(self, name: c_char_p) -> c_int:
+        return self.process.find_pattern(self.number, name)
+
+    def get_pattern_x(self, pat_num):
+        return self.process.get_pattern_x(self.number, pat_num)
+
+    def get_pattern_y(self, pat_num):
+        return self.process.get_pattern_y(self.number, pat_num)
+
+    def get_pattern_tracks(self, pat_num):
+        return self.process.get_pattern_tracks(self.number, pat_num)
+
+    def get_pattern_lines(self, pat_num):
+        return self.process.get_pattern_lines(self.number, pat_num)
+
+    def get_pattern_name(self, pat_num: c_int) -> c_char_p:
+        return self.process.get_pattern_name(self.number, pat_num)
+
+    def get_pattern_data(self, pat_num):
+        return self.process.get_pattern_data(self.number, pat_num)
+
+    def pattern_mute(self, pat_num, mute):
+        with self.locked():
+            return self.process.pattern_mute(self.number, pat_num, mute)
 
 
 __all__ = ["FILENAME_ENCODING", "MAX_SLOTS", "NoSlotsAvailable", "Slot"]
